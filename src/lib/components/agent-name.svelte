90c3ceb051e5352478224211a1763aa407e56c94
nharu-0630 <root@nharu.dev>
2024-12-19 17:22:32 +0900

    [fix] 複数タブ時にエージェント名が異なる問題を修正

--- src/lib/components/agent-name.svelte
+++ src/lib/components/agent-name.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   export let agentIdx: string;
   export let highlight = false;
-
-  const padIdx = agentIdx.padStart(2, "0");
 </script>
 
-<strong class={highlight ? "agent" + padIdx : ""}>Agent[{padIdx}]</strong>
+<strong class={highlight ? "agent" + agentIdx.padStart(2, "0") : ""}
+  >Agent[{agentIdx.padStart(2, "0")}]</strong
+>
 
 <style>
   strong.agent01 {
