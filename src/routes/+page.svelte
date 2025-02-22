1a07ed77b7f2cc2492a9b6013251989ecf937ee1
nharu-0630 <root@nharu.dev>
2025-02-22 22:53:06 +0900

    [add] トップページを追加

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import "../app.css";
+</script>
+
+<svelte:head>
+  <title>aiwolf-nlp-viewer</title>
+</svelte:head>
+
+<main>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">aiwolf-nlp-viewer</h1>
+        <div class="py-6">
+          <button
+            class="btn btn-primary mx-2"
+            on:click={() => (window.location.href = "/realtime")}
+            >リアルタイムログ</button
+          >
+          <button
+            class="btn btn-primary mx-2"
+            on:click={() => (window.location.href = "/archive")}
+            >アーカイブログ</button
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
