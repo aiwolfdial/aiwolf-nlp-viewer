ae3a7b4e03442b0990a313693b4bef034f919141
nharu-0630 <root@nharu.dev>
2025-03-05 00:50:55 +0900

    [update] リンクボタンをミドルクリックに対応

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
-  import { base } from "$app/paths";
   import "../app.css";
 </script>
 
@@ -16,44 +14,29 @@
         <div class="p-2">
           <h2 class="text-xl font-bold">ログビューア</h2>
           <div class="flex flex-row justify-center gap-4 p-2">
-            <button
-              class="btn btn-primary"
-              onclick={() => goto(base + "/realtime")}>リアルタイムログ</button
-            >
-            <button
-              class="btn btn-primary"
-              onclick={() => goto(base + "/archive")}>アーカイブログ</button
-            >
+            <a class="btn btn-primary" href="./realtime">リアルタイムログ</a>
+            <a class="btn btn-primary" href="./archive">アーカイブログ</a>
           </div>
         </div>
         <div class="p-2">
           <h2 class="text-xl font-bold">ソースコード</h2>
           <div class="flex flex-row justify-center gap-4 p-2">
-            <button
+            <a
               class="btn btn-primary"
-              onclick={() =>
-                window.open("https://github.com/kano-lab/aiwolf-nlp-server")}
-              >対戦サーバ</button
+              href="https://github.com/kano-lab/aiwolf-nlp-server">対戦サーバ</a
             >
-            <button
+            <a
               class="btn btn-primary"
-              onclick={() =>
-                window.open("https://github.com/kano-lab/aiwolf-nlp-agent")}
-              >エージェント</button
+              href="https://github.com/kano-lab/aiwolf-nlp-agent"
+              >エージェント</a
             >
           </div>
         </div>
         <div class="p-2">
           <h2 class="text-xl font-bold">その他</h2>
           <div class="flex flex-row justify-center gap-4 p-2">
-            <button
-              class="btn btn-primary"
-              onclick={() => goto(base + "/token")}>トークン生成</button
-            >
-            <button
-              class="btn btn-primary"
-              onclick={() => goto(base + "/statistics")}>統計情報</button
-            >
+            <a class="btn btn-primary" href="./token">トークン生成</a>
+            <a class="btn btn-primary" href="./statistics">統計情報</a>
           </div>
         </div>
       </div>
