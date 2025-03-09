6d6b43949993bb0794d74c4e0cdce283d7cc8962
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2025-03-09 17:30:42 +0900

    [update] レイアウトを修正

--- src/routes/agent/+page.svelte
+++ src/routes/agent/+page.svelte
@@ -5,6 +5,7 @@
   import {
     Request,
     RequestJA,
+    Role,
     RoleJA,
     Species,
     SpeciesJA,
@@ -13,7 +14,6 @@
     type Info,
     type Judge,
     type Packet,
-    type Role,
     type Setting,
     type Talk,
   } from "$lib/types/agent";
@@ -148,41 +148,33 @@
 
 <main class="h-screen flex flex-col bg-base-300">
   <Navbar />
-  <div class="flex-1 flex flex-col h-full overflow-hidden">
-    <div class="flex flex-row h-full">
-      <div class="w-1/3 flex flex-col overflow-y-auto">
-        <pre class="font-bold text-3xl p-2">{$info?.agent ?? "未接続"} {RoleJA[
-            $role!
-          ]}</pre>
-        {#if $request !== null}
-          <pre
-            class="bg-primary text-primary-content text-2xl p-2 m-2">{$info !==
-            null
-              ? $info.day + "日目"
-              : "ゲーム外"} {RequestJA[$request!]}</pre>
-        {/if}
-        {#if $info !== null}
-          <div class="p-2">
+  {#if $info !== null}
+    <div class="overflow-y-hidden flex grow overflox-x-auto gap-4 p-4">
+      <div class="flex-[0_0_400px] rounded-lg bg-base-200">
+        <div class="flex flex-col h-full p-4">
+          <h2 class="text-xl font-bold pb-2">エージェント</h2>
+          <div class="grow overflow-y-auto pr-4">
+            <h2 class="text-lg font-bold">リクエスト</h2>
+            {#if $request !== null}
+              <pre class="text-lg">{$info !== null
+                  ? $info.day + "日目"
+                  : "ゲーム外"} {RequestJA[$request!]}</pre>
+            {/if}
+            <h2 class="text-lg font-bold pt-2">ステータス</h2>
             {#each Object.entries($info.statusMap ?? {}) as [key, value]}
               {#if value === Status.ALIVE}
-                <div class="bg-info text-info-content flex flex-row gap-2 p-2">
-                  <pre class="font-bold text-2xl">{key}</pre>
-                  <pre class="font-bold text-2xl">{RoleJA[
-                      ($info.roleMap ?? {})[key]
-                    ] ?? ""}</pre>
-                  <pre class="font-bold text-2xl ml-auto">{StatusJA[
-                      value
-                    ]}</pre>
+                <div class="bg-info text-info-content flex flex-row gap-2">
+                  <pre class="text-lg">{key}</pre>
+                  <pre class="text-lg">{RoleJA[($info.roleMap ?? {})[key]] ??
+                      ""}</pre>
+                  <pre class="text-lg ml-auto">{StatusJA[value]}</pre>
                 </div>
               {:else}
-                <div
-                  class="bg-error text-error-content flex flex-row gap-2 p-2"
-                >
-                  <pre class="font-bold text-2xl">{key}</pre>
-                  <pre class="font-bold text-2xl">{RoleJA[
-                      ($info.roleMap ?? {})[key]
-                    ] ?? ""}</pre>
-                  <pre class="font-bold text-2xl ml-auto">{StatusJA[
+                <div class="bg-error text-error-content flex flex-row gap-2">
+                  <pre class="text-lg">{key}</pre>
+                  <pre class="text-lg">{RoleJA[($info.roleMap ?? {})[key]] ??
+                      ""}</pre>
+                  <pre class="text-lg ml-auto">{StatusJA[
                       value
                     ]}{$executedAgents.includes(key)
                       ? " (追放)"
@@ -192,224 +184,222 @@
                 </div>
               {/if}
             {/each}
-          </div>
-          {#if $mediumResults.length > 0}
-            <div class="p-2">
-              <h2 class="font-bold text-lg">霊能結果</h2>
+            {#if $mediumResults.length > 0}
+              <h2 class="text-lg font-bold pt-2">霊能結果</h2>
               {#each $mediumResults as { day, target, result }}
                 {#if result !== Species.HUMAN}
-                  <div
-                    class="bg-error text-error-content flex flex-row gap-2 p-2"
-                  >
-                    <pre class="font-bold text-2xl">{target}</pre>
-                    <pre class="font-bold text-2xl">({day}日目)</pre>
-                    <pre class="font-bold text-2xl ml-auto">{SpeciesJA[
-                        result
-                      ]}</pre>
+                  <div class="bg-error text-error-content flex flex-row gap-2">
+                    <pre class="text-lg">{target}</pre>
+                    <pre class="text-lg">({day}日目)</pre>
+                    <pre class="text-lg ml-auto">{SpeciesJA[result]}</pre>
                   </div>
                 {:else}
                   <div
-                    class="bg-success text-success-content flex flex-row gap-2 p-2"
+                    class="bg-success text-success-content flex flex-row gap-2"
                   >
-                    <pre class="font-bold text-2xl">{target}</pre>
-                    <pre class="font-bold text-2xl">({day}日目)</pre>
-                    <pre class="font-bold text-2xl ml-auto">{SpeciesJA[
-                        result
-                      ]}</pre>
+                    <pre class="text-lg">{target}</pre>
+                    <pre class="text-lg">({day}日目)</pre>
+                    <pre class="text-lg ml-auto">{SpeciesJA[result]}</pre>
                   </div>
                 {/if}
               {/each}
-            </div>
-          {/if}
-          {#if $divineResults.length > 0}
-            <div class="p-2">
-              <h2 class="font-bold text-lg">占い結果</h2>
+            {/if}
+            {#if $divineResults.length > 0}
+              <h2 class="font-bold text-lg pt-2">占い結果</h2>
               {#each $divineResults as { day, target, result }}
                 {#if result !== Species.HUMAN}
-                  <div
-                    class="bg-error text-error-content flex flex-row gap-2 p-2"
-                  >
-                    <pre class="font-bold text-2xl">{target}</pre>
-                    <pre class="font-bold text-2xl">({day}日目)</pre>
-                    <pre class="font-bold text-2xl ml-auto">{SpeciesJA[
-                        result
-                      ]}</pre>
+                  <div class="bg-error text-error-content flex flex-row gap-2">
+                    <pre class="text-lg">{target}</pre>
+                    <pre class="text-lg">({day}日目)</pre>
+                    <pre class="text-lg ml-auto">{SpeciesJA[result]}</pre>
                   </div>
                 {:else}
                   <div
-                    class="bg-success text-success-content flex flex-row gap-2 p-2"
+                    class="bg-success text-success-content flex flex-row gap-2"
                   >
-                    <pre class="font-bold text-2xl">{target}</pre>
-                    <pre class="font-bold text-2xl">({day}日目)</pre>
-                    <pre class="font-bold text-2xl ml-auto">{SpeciesJA[
-                        result
-                      ]}</pre>
+                    <pre class="text-lg">{target}</pre>
+                    <pre class="text-lg">({day}日目)</pre>
+                    <pre class="text-lg ml-auto">{SpeciesJA[result]}</pre>
                   </div>
                 {/if}
               {/each}
+            {/if}
+          </div>
+        </div>
+      </div>
+      <div class="flex-[0_0_600px] rounded-lg bg-base-200">
+        <div class="flex flex-col h-full p-4">
+          <h2 class="text-xl font-bold pb-2">トーク履歴</h2>
+          <div class="grow overflow-y-auto pr-4">
+            {#if $talkHistory.length > 0}
+              {@const days = [...new Set($talkHistory.map((t) => t.day))].sort(
+                (a, b) => b - a
+              )}
+              <div class="tabs tabs-border">
+                {#each days as day}
+                  <input
+                    type="radio"
+                    name="talk_days"
+                    class="tab"
+                    checked={day === days[0]}
+                    aria-label={`${day}日目`}
+                  />
+                  <div class="tab-content my-4">
+                    {#each $talkHistory.filter((t) => t.day === day) as { agent, idx, text, skip, over }}
+                      <div class="chat chat-start">
+                        <div class="chat-image avatar avatar-placeholder">
+                          <div
+                            class="bg-neutral text-neutral-content w-12 rounded-full"
+                          >
+                            <span class="text-2xl">
+                              {Number(agent.match(/Agent\[(\d+)\]/)?.[1]) ?? ""}
+                            </span>
+                          </div>
+                        </div>
+                        <div class="chat-header"></div>
+                        <div
+                          class="chat-bubble bg-base-100 text-pretty break-all"
+                        >
+                          {text}
+                        </div>
+                        <pre class="chat-footer opacity-50">{idx}</pre>
+                      </div>
+                    {/each}
+                  </div>
+                {/each}
+              </div>
+            {/if}
+          </div>
+        </div>
+      </div>
+      {#if $role === Role.WEREWOLF}
+        <div class="flex-[0_0_600px] rounded-lg bg-base-200">
+          <div class="flex flex-col h-full p-4">
+            <h2 class="text-xl font-bold pb-2">囁き履歴</h2>
+            <div class="grow overflow-y-auto pr-4">
+              {#if $whisperHistory.length > 0}
+                {@const days = [
+                  ...new Set($whisperHistory.map((t) => t.day)),
+                ].sort((a, b) => b - a)}
+                <div class="tabs tabs-border">
+                  {#each days as day}
+                    <input
+                      type="radio"
+                      name="whisper_days"
+                      class="tab"
+                      checked={day === days[0]}
+                      aria-label={`${day}日目`}
+                    />
+                    <div class="tab-content my-4">
+                      {#each $whisperHistory.filter((t) => t.day === day) as { agent, idx, text, skip, over }}
+                        <div class="chat chat-start">
+                          <div class="chat-image avatar avatar-placeholder">
+                            <div
+                              class="bg-neutral text-neutral-content w-12 rounded-full"
+                            >
+                              <span class="text-2xl">
+                                {Number(agent.match(/Agent\[(\d+)\]/)?.[1]) ??
+                                  ""}
+                              </span>
+                            </div>
+                          </div>
+                          <div class="chat-header"></div>
+                          <div
+                            class="chat-bubble bg-base-100 text-pretty break-all"
+                          >
+                            {text}
+                          </div>
+                          <pre class="chat-footer opacity-50">{idx}</pre>
+                        </div>
+                      {/each}
+                    </div>
+                  {/each}
+                </div>
+              {/if}
             </div>
-          {/if}
-        {/if}
-      </div>
-      <div class="w-1/3 flex flex-col overflow-y-auto">
-        <h2 class="font-bold text-lg p-2">トーク履歴</h2>
-        {#if $talkHistory.length > 0}
-          {@const days = [...new Set($talkHistory.map((t) => t.day))].sort(
-            (a, b) => b - a
-          )}
-          <div class="tabs tabs-border flex-none">
-            {#each days as day}
-              <input
-                type="radio"
-                name="talk_days"
-                class="tab"
-                checked={day === days[0]}
-                aria-label={`${day}日目`}
-              />
-              <div class="tab-content overflow-y-auto h-full">
-                {#each $talkHistory.filter((t) => t.day === day) as { agent, idx, text, skip, over }}
-                  <div class="chat chat-start">
-                    <div class="chat-image avatar avatar-placeholder">
-                      <div
-                        class="bg-neutral text-neutral-content w-12 rounded-full"
-                      >
-                        <span class="text-2xl">
-                          {Number(agent.match(/Agent\[(\d+)\]/)?.[1]) ?? ""}
-                        </span>
-                      </div>
-                    </div>
-                    <div class="chat-header"></div>
-                    <div class="chat-bubble bg-base-100 text-pretty break-all">
-                      {text}
-                    </div>
-                    <pre class="chat-footer opacity-50">{idx}</pre>
-                  </div>
-                {/each}
-              </div>
-            {/each}
           </div>
-        {/if}
-      </div>
-      <div class="w-1/3 flex flex-col overflow-y-auto">
-        <h2 class="font-bold text-lg">囁き履歴</h2>
-        {#if $whisperHistory.length > 0}
-          {@const days = [...new Set($whisperHistory.map((t) => t.day))].sort(
-            (a, b) => b - a
-          )}
-          <div class="tabs tabs-border flex-none">
-            {#each days as day}
-              <input
-                type="radio"
-                name="whisper_days"
-                class="tab"
-                checked={day === days[0]}
-                aria-label={`${day}日目`}
-              />
-              <div class="tab-content overflow-y-auto h-full">
-                {#each $whisperHistory.filter((t) => t.day === day) as { agent, idx, text, skip, over }}
-                  <div class="chat chat-start">
-                    <div class="chat-image avatar avatar-placeholder">
-                      <div
-                        class="bg-neutral text-neutral-content w-12 rounded-full"
-                      >
-                        <span class="text-2xl">
-                          {Number(agent.match(/Agent\[(\d+)\]/)?.[1]) ?? ""}
-                        </span>
-                      </div>
-                    </div>
-                    <div class="chat-header"></div>
-                    <div class="chat-bubble bg-base-100 text-pretty break-all">
-                      {text}
-                    </div>
-                    <pre class="chat-footer opacity-50">{idx}</pre>
-                  </div>
-                {/each}
-              </div>
-            {/each}
-          </div>
-        {/if}
-      </div>
+        </div>
+      {/if}
     </div>
-  </div>
-
-  {#if $remain !== null}
-    <div class="flex-none bg-base-200">
-      <div class="flex gap-2 items-center mx-4 mt-4">
-        <span class="countdown font-mono text-2xl">
-          {#if $remain > 60000}
+    {#if $remain !== null}
+      <div class="flex-none bg-base-200">
+        <div class="flex gap-2 items-center mx-4 mt-4">
+          <span class="countdown font-mono text-2xl">
+            {#if $remain > 60000}
+              <span
+                style="--value:{Math.floor($remain / 60000)};"
+                aria-live="polite"
+                aria-label={Math.floor($remain / 60000).toString()}
+                >{Math.floor($remain / 60000)}</span
+              >m
+            {/if}
             <span
-              style="--value:{Math.floor($remain / 60000)};"
+              style="--value:{Math.floor(($remain % 60000) / 1000)};"
               aria-live="polite"
-              aria-label={Math.floor($remain / 60000).toString()}
-              >{Math.floor($remain / 60000)}</span
-            >m
-          {/if}
-          <span
-            style="--value:{Math.floor(($remain % 60000) / 1000)};"
-            aria-live="polite"
-            aria-label={Math.floor(($remain % 60000) / 1000).toString()}
-            >{Math.floor(($remain % 60000) / 1000)}</span
-          >s
-        </span>
-        {#if $request === Request.VOTE || $request === Request.DIVINE || $request === Request.GUARD || $request === Request.ATTACK}
-          {#each Object.entries($info?.statusMap ?? {}) as [key, value]}
-            {#if value === Status.ALIVE && key !== $info?.agent}
-              <button class="btn" onclick={() => ($message = key)}>
-                {key}
+              aria-label={Math.floor(($remain % 60000) / 1000).toString()}
+              >{Math.floor(($remain % 60000) / 1000)}</span
+            >s
+          </span>
+          {#if $request === Request.VOTE || $request === Request.DIVINE || $request === Request.GUARD || $request === Request.ATTACK}
+            {#each Object.entries($info?.statusMap ?? {}) as [key, value]}
+              {#if value === Status.ALIVE && key !== $info?.agent}
+                <button class="btn" onclick={() => ($message = key)}>
+                  {key}
+                </button>
+              {/if}
+            {/each}
+          {:else}
+            {#if $setting?.maxSkip ?? 0 > 0}
+              <button
+                class="btn btn-square"
+                onclick={() => ($message = "Skip")}
+                aria-label="Skip"
+              >
+                <iconify-icon icon="mdi:arrow-u-down-right-bold"></iconify-icon>
               </button>
             {/if}
-          {/each}
-        {:else}
-          {#if $setting?.maxSkip ?? 0 > 0}
             <button
               class="btn btn-square"
-              onclick={() => ($message = "Skip")}
-              aria-label="Skip"
+              onclick={() => ($message = "Over")}
+              aria-label="Over"
             >
-              <iconify-icon icon="mdi:arrow-u-down-right-bold"></iconify-icon>
+              <iconify-icon icon="mdi:skip-forward"></iconify-icon>
             </button>
           {/if}
+          <input
+            type="text"
+            class="input flex-1"
+            bind:value={$message}
+            list="agents"
+          />
+          <datalist id="agents">
+            {#each Object.entries($info?.statusMap ?? {}) as [key, value]}
+              {#if value === Status.ALIVE && key !== $info?.agent}
+                <option value={key}></option>
+              {/if}
+            {/each}
+          </datalist>
           <button
             class="btn btn-square"
-            onclick={() => ($message = "Over")}
-            aria-label="Over"
+            onclick={() => {
+              agentSocketState.send($message);
+              $message = "";
+            }}
+            aria-label="Send"
           >
-            <iconify-icon icon="mdi:skip-forward"></iconify-icon>
+            <iconify-icon icon="mdi:send"></iconify-icon>
           </button>
-        {/if}
-        <input
-          type="text"
-          class="input flex-1"
-          bind:value={$message}
-          list="agents"
-        />
-        <datalist id="agents">
-          {#each Object.entries($info?.statusMap ?? {}) as [key, value]}
-            {#if value === Status.ALIVE && key !== $info?.agent}
-              <option value={key}></option>
-            {/if}
-          {/each}
-        </datalist>
-        <button
-          class="btn btn-square"
-          onclick={() => {
-            agentSocketState.send($message);
-            $message = "";
-          }}
-          aria-label="Send"
-        >
-          <iconify-icon icon="mdi:send"></iconify-icon>
-        </button>
+        </div>
+        <div class="mx-4 mb-2">
+          <progress
+            class="progress"
+            value={$remain !== null
+              ? ($remain / ($setting?.actionTimeout ?? 60000)) * 100
+              : 0}
+            max="100"
+          ></progress>
+        </div>
       </div>
-      <div class="mx-4 mb-2">
-        <progress
-          class="progress"
-          value={$remain !== null
-            ? ($remain / ($setting?.actionTimeout ?? 60000)) * 100
-            : 0}
-          max="100"
-        ></progress>
-      </div>
-    </div>
+    {/if}
   {/if}
 </main>
