3cc91bfeb1c05e30e69aa56be26c4d8a251f2f80
nharu-0630 <root@nharu.dev>
2025-04-13 19:07:35 +0900

    Updates

--- _app/immutable/chunks/DRadVfQi.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1tkty3h)==null?void 0:s.base)??"/aiwolf-nlp-viewer";var t;const a=((t=globalThis.__sveltekit_1tkty3h)==null?void 0:t.assets)??e;export{a,e as b};
