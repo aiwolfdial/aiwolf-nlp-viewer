79223ca3ee7041cdbe97fb39012f1077386e6cb5
nharu <nharu@nharus-MacBook-Air-M4.local>
2025-05-11 22:55:50 +0900

    Updates

--- _app/immutable/chunks/dKdZw_CZ.js
+++
@@ -1,1 +0,0 @@
-var e;const a=((e=globalThis.__sveltekit_1eeugye)==null?void 0:e.base)??"/aiwolf-nlp-viewer";var s;const t=((s=globalThis.__sveltekit_1eeugye)==null?void 0:s.assets)??a;export{t as a,a as b};
