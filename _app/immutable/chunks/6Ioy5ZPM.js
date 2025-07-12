15b2366e0265847ab771d4ca33f1ecbc58c95f10
nharu-0630 <root@nharu.dev>
2025-07-12 23:24:36 +0900

    Updates

---
+++ _app/immutable/chunks/6Ioy5ZPM.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_nqw19t)==null?void 0:s.base)??"/aiwolf-nlp-viewer";var e;const a=((e=globalThis.__sveltekit_nqw19t)==null?void 0:e.assets)??t;export{a,t as b};
