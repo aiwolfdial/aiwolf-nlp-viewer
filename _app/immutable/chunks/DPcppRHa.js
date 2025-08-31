dd88b1afedcb74ef3365c61b0967f966555161fb
nharu-0630 <root@nharu.dev>
2025-08-31 23:18:22 +0900

    Updates

--- _app/immutable/chunks/DPcppRHa.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_g2ktvc?.base??"/aiwolf-nlp-viewer",e=globalThis.__sveltekit_g2ktvc?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/DyEBYryk.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_5k16n?.base??"/aiwolf-nlp-viewer",e=globalThis.__sveltekit_5k16n?.assets??s;export{e as a,s as b};
