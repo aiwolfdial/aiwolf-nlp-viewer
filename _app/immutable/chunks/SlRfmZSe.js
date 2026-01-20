a240c1a6dc260cba13d427b905044ae24a764539
nharu-0630 <root@nharu.dev>
2026-01-20 17:09:26 +0900

    Updates

--- _app/immutable/chunks/SlRfmZSe.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_16ej7g7?.base??"/aiwolf-nlp-viewer",e=globalThis.__sveltekit_16ej7g7?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/ySXC2e1p.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1vwhyxo?.base??"/aiwolf-nlp-viewer",e=globalThis.__sveltekit_1vwhyxo?.assets??s??"";export{e as a,s as b};
